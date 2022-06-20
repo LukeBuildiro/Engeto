cf2791a6e627dc37e96b551fb5cc4c61b577fa17
Lukas <lukaspolach@gmail.com>
2022-06-20 21:02:25 +0200

    5.ukol

--- SQL project.sql
+++ SQL project.sql
@@ -3,7 +3,13 @@ SELECT
 	industry_branch_code,
 	payroll_year,
 	payroll_quarter,
-	cpib.name
+	cpib.name,
+(value/(SELECT
+	AVG (value)
+	FROM czechia_payroll cp1
+	WHERE industry_branch_code = cp.industry_branch_code
+	AND payroll_year = cp.payroll_year -1
+	GROUP BY payroll_year)) AS prev_year
 FROM
 	czechia_payroll cp
 JOIN czechia_payroll_industry_branch cpib 
@@ -95,42 +101,6 @@ ORDER BY
 
 -- 3. ukol
 
--- Mato
-
-SELECT
-	category_code,date_from,
-	((SELECT
-		value
-	FROM
-		czechia_price cp2
-	WHERE
-		date_from <= 
-DATE_ADD(cp.date_from, INTERVAL 1 YEAR)
-		AND date_to >= 
-DATE_ADD(cp.date_from, INTERVAL 1 YEAR)
-		AND category_code = cp.category_code
-		AND region_code IS NULL
-	LIMIT 1)/ value)*100-100 AS ratio
-FROM
-	czechia_price cp
-WHERE
-	region_code IS NULL
-AND ((SELECT
-		value
-	FROM
-		czechia_price cp2
-	WHERE
-		date_from <= 
-DATE_ADD (cp.date_from, INTERVAL 1 YEAR)
-		AND date_to >= 
-DATE_ADD(cp.date_from, INTERVAL 1 YEAR)
-		AND category_code = cp.category_code
-		AND region_code IS NULL
-	LIMIT 1)/ value)*100-100 IS NOT NULL
-ORDER BY ratio ASC
-
--- Lukas 
-
 
 SELECT
 	(cur.value / prev.value)* 100-100 AS ratio,
@@ -217,12 +187,225 @@ GROUP BY
 ORDER BY
 	ratio ASC 
 	
+	
+-- 4. ukol
+	
+SELECT FORMAT(AVG (value),2), YEAR (date_from) 
+FROM czechia_price cp 
+WHERE region_code IS NULL
+GROUP BY
+	YEAR (date_from) 
+ORDER BY 
+	YEAR (date_from);
+
+
+-- check food values
+
+SELECT COUNT(value)  
+FROM czechia_price cp 
+WHERE region_code IS NULL 
+AND YEAR (date_from) = 2006
+
+
+SELECT SUM(value)  
+FROM czechia_price cp 
+WHERE region_code IS NULL 
+AND YEAR (date_from) = 2006
+
+SELECT 55911.33000000001/1253;
+
+
+SELECT
+	FORMAT(AVG (value), 2),
+	YEAR (date_from),
+	FORMAT(AVG (value)/(
+	SELECT
+		AVG (value)
+	FROM
+		czechia_price cp2
+	WHERE
+		region_code IS NULL
+	AND YEAR (date_from) = YEAR (cp.date_from)-1
+	GROUP BY
+		YEAR (date_from)
+	LIMIT 1)*100-100,2) AS increase_per_year
+FROM
+	czechia_price cp
+WHERE
+	region_code IS NULL
+GROUP BY
+	YEAR (date_from)
+ORDER BY 
+	YEAR (date_from)
+	
+	
+-- salaries
+	
+SELECT
+	FORMAT(AVG (value), 2), payroll_year,
+	FORMAT(AVG (value)/(
+	SELECT
+		AVG (value)
+	FROM
+		czechia_payroll cp2 
+	WHERE
+		value_type_code = 5958
+	AND cp2.payroll_year = cp.payroll_year-1
+	GROUP BY
+		payroll_year 
+	LIMIT 1)*100-100,2) AS increase_per_year
+FROM
+	czechia_payroll cp 
+WHERE
+	 value_type_code = 5958
+GROUP BY
+	payroll_year 
+ORDER BY 
+	payroll_year
+	
+-- increase over 10%
+
+
+SELECT
+	food_increase.increase_per_year AS food_inc,
+	salary_increase.increase_per_year AS salary_inc,
+	payroll_year,
+	food_increase.increase_per_year/salary_increase.increase_per_year*100-100 AS avg_percentage_increase
+FROM
+	(
+	SELECT
+		AVG (value),
+		YEAR (date_from) AS years,
+		AVG (value)/(
+	SELECT
+		AVG (value)
+	FROM
+		czechia_price cp2
+	WHERE
+		region_code IS NULL
+	AND YEAR (date_from) = YEAR (cp.date_from)-1
+	GROUP BY
+		YEAR (date_from)
+	LIMIT 1)* 100-100 AS increase_per_year
+	FROM
+		czechia_price cp
+	WHERE
+		region_code IS NULL
+	GROUP BY
+		YEAR (date_from)
+	ORDER BY
+		YEAR (date_from)) AS food_increase
+JOIN	
+(
+	SELECT
+		AVG (value),
+		payroll_year,
+		AVG (value)/(
+	SELECT
+		AVG (value)
+	FROM
+		czechia_payroll cp2 
+	WHERE
+		value_type_code = 5958
+	AND cp2.payroll_year = cp.payroll_year-1
+	GROUP BY
+		payroll_year 
+	LIMIT 1)* 100-100 AS increase_per_year
+	FROM
+		czechia_payroll cp
+	WHERE
+		value_type_code = 5958
+	GROUP BY
+		payroll_year
+	ORDER BY
+		payroll_year) AS salary_increase
+ON
+	food_increase.years = salary_increase.payroll_year;
 
 
 
 
 	
+==== 5.ukol
 
+SELECT
+	food_increase.increase_per_year AS food_inc,
+	salary_increase.increase_per_year AS salary_inc,
+	payroll_year,
+	food_increase.increase_per_year / salary_increase.increase_per_year * 100-100 AS avg_percentage_increase,
+	hdp,
+	hdp_perc_increase
+FROM
+	(
+	SELECT
+		AVG (value),
+		YEAR (date_from) AS years,
+		AVG (value)/(
+		SELECT
+			AVG (value)
+		FROM
+			czechia_price cp2
+		WHERE
+			region_code IS NULL
+			AND YEAR (date_from) = YEAR (cp.date_from)-1
+		GROUP BY
+			YEAR (date_from)
+		LIMIT 1)* 100-100 AS increase_per_year
+	FROM
+		czechia_price cp
+	WHERE
+		region_code IS NULL
+	GROUP BY
+		YEAR (date_from)
+	ORDER BY
+		YEAR (date_from)) AS food_increase
+JOIN	
+(
+	SELECT
+		AVG (value),
+		payroll_year,
+		AVG (value)/(
+		SELECT
+			AVG (value)
+		FROM
+			czechia_payroll cp2
+		WHERE
+			value_type_code = 5958
+			AND cp2.payroll_year = cp.payroll_year-1
+		GROUP BY
+			payroll_year
+		LIMIT 1)* 100-100 AS increase_per_year
+	FROM
+		czechia_payroll cp
+	WHERE
+		value_type_code = 5958
+	GROUP BY
+		payroll_year
+	ORDER BY
+		payroll_year) AS salary_increase
+ON
+	food_increase.years = salary_increase.payroll_year
+JOIN
+(
+	SELECT
+		e.year AS year,
+		e.GDP AS hdp,
+		e.gdp/(
+		SELECT
+			hdp AS hdp
+		FROM
+			economies e2
+		WHERE
+			country = "Czech Republic"
+			AND e.`year` -1 = e2.`year`) AS hdp_perc_increase
+	FROM
+		economies e
+	WHERE
+		country = "Czech Republic") AS hdp
+ON
+	food_increase.years = hdp.year
+ORDER BY
+	years ASC;
 
 
 
